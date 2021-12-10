a31424acf2c06438050f699ee5077f71b80abc1d
akinniyThomas <thomas.akinniyi@decagon.dev>
2021-12-10 22:27:18 +0100

    model done

---
+++ TODO_APP/TodoItem.swift
@@ -0,0 +1,15 @@
+//
+//  TodoItem.swift
+//  TODO_APP
+//
+//  Created by Decagon on 10/12/2021.
+//
+
+import Foundation
+
+
+struct TodoItem: Codable {
+    let id: Int
+    let text: String
+    let isDone: Bool
+}
