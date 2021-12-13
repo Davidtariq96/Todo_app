2c536f0f6b2b6a0ace7fe399066f86d910d43d78
SharonB9 <92518636+SharonB9@users.noreply.github.com>
2021-12-13 16:40:48 +0100

    constraints+cell updated

--- TODO_APP/TODO_APP/View/Constraints/Constraints+Cell.swift
+++ TODO_APP/TODO_APP/View/Constraints/Constraints+Cell.swift
@@ -1,42 +1,39 @@
-
 import UIKit
 
 extension TodoViewCell{
     
     func configureCell(){
         addSubview(tickButton)
-        addSubview(todoTextField)
+        addSubview(todoLabel)
         addSubview(deleteButton)
-        
         configureTickButton()
         configureTodoLabel()
         configureDeleteButton()
-        
         setTickButtonConstraints()
         setTodoLabelConstraints()
+        configureDeleteButton()
         setDeleteButtonConstraints()
     }
-    
     func setTickButtonConstraints(){
         tickButton.translatesAutoresizingMaskIntoConstraints = false
         tickButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         tickButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
-        tickButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        tickButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
         tickButton.widthAnchor.constraint(equalTo: tickButton.heightAnchor, multiplier: 16/9).isActive = true
     }
     func setTodoLabelConstraints(){
-       todoTextField.translatesAutoresizingMaskIntoConstraints = false
-        todoTextField.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        todoTextField.leadingAnchor.constraint(equalTo: tickButton.trailingAnchor, constant: 8).isActive = true
-        todoTextField.trailingAnchor.constraint(equalTo: deleteButton.leadingAnchor).isActive = true
-        todoTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        todoTextField.widthAnchor.constraint(equalTo: todoTextField.heightAnchor, multiplier: 16/9).isActive = true
+        todoLabel.translatesAutoresizingMaskIntoConstraints = false
+        todoLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        todoLabel.leadingAnchor.constraint(equalTo: tickButton.trailingAnchor, constant: 30).isActive = true
+        todoLabel.trailingAnchor.constraint(equalTo: deleteButton.leadingAnchor).isActive = true
+        todoLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        todoLabel.widthAnchor.constraint(equalTo: todoLabel.heightAnchor, multiplier: 16/9).isActive = true
     }
     func setDeleteButtonConstraints(){
         deleteButton.translatesAutoresizingMaskIntoConstraints = false
         deleteButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        deleteButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        deleteButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        deleteButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        deleteButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
         deleteButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
     }
 }
